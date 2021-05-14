416c7bdd51f709ceb698f647ca6c8088b667a827
Dulanjali Liyanage <dliyanage@mitrai.com>
2021-05-14 11:35:22 +0530

    areas

---
+++ ShoppingCartDemo/Areas/Identity/Data/ShoppingCartDemoContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCartDemo.Areas.Identity.Data;
+using ShoppingCartDemo.Models;
+
+namespace ShoppingCartDemo.Data
+{
+    public class ShoppingCartDemoContext : IdentityDbContext<ApplicationUser>
+    {
+        
+        public ShoppingCartDemoContext(DbContextOptions<ShoppingCartDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Item { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+        
+    }
+}
---
+++ ShoppingCartDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCartDemo.Areas.Identity.Data;
+using ShoppingCartDemo.Data;
+
+[assembly: HostingStartup(typeof(ShoppingCartDemo.Areas.Identity.IdentityHostingStartup))]
+namespace ShoppingCartDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ShoppingCartDemoContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ShoppingCartDemoContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => 
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                }).AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ShoppingCartDemoContext>();
+            });
+        }
+    }
+}
